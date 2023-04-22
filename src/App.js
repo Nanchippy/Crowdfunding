dda7f36270b8a9bc0b4528bee31ffb01cb39493a
Nanchippy <nandhanaaregi@gmail.com>
2023-04-22 21:56:38 +0530

    Created contribute page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+function ContributeEther() {
+  const [etherAmount, setEtherAmount] = useState("");
+
+  function showAlert(message) {
+    const alert = document.getElementById("alert");
+    alert.innerHTML = `<span class="closebtn" onclick='this.parentElement.style.display="none";'>&times;</span>${message}`;
+    alert.style.display = "block";
+  }
+
+  function validateForm(event) {
+    event.preventDefault();
+    if (etherAmount < 0.5) {
+      showAlert("Error: Contribution must be at least 0.5 ether");
+    } else {
+      // submit the form
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={validateForm}>
+        <h1>Enter Amount(ETH)</h1>
+        <label htmlFor="etherAmount"></label>
+        <input
+          type="number"
+          id="etherAmount"
+          name="etherAmount"
+          step="0.0000001"
+          placeholder="Enter amount to contribute"
+          required
+          className="no-spinner"
+          value={etherAmount}
+          onChange={(e) => setEtherAmount(e.target.value)}
+        />
+        <div id="alert" className="alert" style={{ display: "none" }}>
+          <span
+            className="closebtn"
+            onClick={(e) =>
+              (e.target.parentElement.style.display = "none")
+            }
+          >
+            &times;
+          </span>
+          <p id="error"></p>
+        </div>
+        <div style={{ textAlign: "right" }}>
+          <input type="button" value="Cancel" onClick={() => window.history.back()} />
+          <input type="submit" value="Contribute" />
+        </div>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default ContributeEther;
